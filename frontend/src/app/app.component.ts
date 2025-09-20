623a2cce7de69fe3d8d60c1d329441e8d3b760c8
HTA Sistemas <htasistemas@gmail.com>
2025-09-20 00:30:56 -0300

    Corrigir declaracoes de componentes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   standalone: false,
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  showTopNav = false;
+
+  constructor(private router: Router) {
+    this.showTopNav = !this.router.url.startsWith('/login');
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.showTopNav = !event.urlAfterRedirects.startsWith('/login');
+      });
+  }
+}
