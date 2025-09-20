028c6b22412b021acc87315d34135bb7889f8a45
HTA Sistemas <htasistemas@gmail.com>
2025-09-19 23:47:57 -0300

    Ajusta componentes para modo com NgModule

--- frontend/src/app/modules/login/login.component.ts
+++ frontend/src/app/modules/login/login.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 @Component({
+  standalone: false,
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
