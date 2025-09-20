dcbb6e1b69e7593ba468f7e949d5f85f753cd154
HTA Sistemas <htasistemas@gmail.com>
2025-09-20 00:51:16 -0300

    Adiciona tela de nova família em Angular

--- frontend/src/app/modules/familias/familias.module.ts
+++ frontend/src/app/modules/familias/familias.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { FamiliasComponent } from './familias.component';
+import { NovaFamiliaComponent } from './nova-familia/nova-familia.component';
 
-const routes: Routes = [{ path: '', component: FamiliasComponent }];
+const routes: Routes = [
+  { path: '', component: FamiliasComponent },
+  { path: 'nova', component: NovaFamiliaComponent }
+];
 
 @NgModule({
-  declarations: [FamiliasComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)]
+  declarations: [FamiliasComponent, NovaFamiliaComponent],
+  imports: [CommonModule, FormsModule, RouterModule.forChild(routes)]
 })
 export class FamiliasModule {}
