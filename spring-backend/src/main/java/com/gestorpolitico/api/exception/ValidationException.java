4baf3e5f3f2df599f4a9f518ea90ed3dc056403d
HTA Sistemas <htasistemas@gmail.com>
2025-09-21 12:57:01 -0300

    Adiciona handler global no Spring

---
+++ spring-backend/src/main/java/com/gestorpolitico/api/exception/ValidationException.java
@@ -0,0 +1,16 @@
+package com.gestorpolitico.api.exception;
+
+import java.util.List;
+
+public class ValidationException extends RuntimeException {
+  private final List<String> errors;
+
+  public ValidationException(List<String> errors) {
+    super("Dados inválidos");
+    this.errors = errors;
+  }
+
+  public List<String> getErrors() {
+    return errors;
+  }
+}
