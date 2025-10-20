10a8d26d06b3ca3de6544180ec03e41c420cae07
HTA Sistemas <htasistemas@gmail.com>
2025-10-19 22:06:54 -0300

    Liberaliza CORS para IPs

--- backend-java/src/main/java/com/gestorpolitico/config/WebConfig.java
+++ backend-java/src/main/java/com/gestorpolitico/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**")
-      .allowedOrigins("http://localhost:4200")
+      .allowedOriginPatterns("http://*", "https://*")
       .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
       .allowedHeaders("*")
       .allowCredentials(false);
